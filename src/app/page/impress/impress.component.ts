0caf07a1812b48a8a807661b4ad919ebf3873270
Maria Luisa <122819130+malurodrz@users.noreply.github.com>
2025-01-02 23:44:43 -0300

    pagina de imprensa

--- src/app/page/impress/impress.component.ts
+++ src/app/page/impress/impress.component.ts
@@ -3,10 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-impress',
   standalone: false,
-  
+
   templateUrl: './impress.component.html',
-  styleUrl: './impress.component.scss'
+  styleUrls: ['./impress.component.scss', '../../component/header/header.component.scss'],
 })
 export class ImpressComponent {
-
+  images = [
+    { src: 'assets/img/hotel.png', alt: 'Image 1', title: 'Empresa de hotéis de Luxo é destaque 2024', site: 'Globo' },
+    { src: 'assets/img/hotel.png', alt: 'Image 2', title: 'Empresa de hotéis de Luxo é destaque 2024', site: 'Globo' },
+    { src: 'assets/img/hotel.png', alt: 'Image 3', title: 'Empresa de hotéis de Luxo é destaque 2024', site: 'Globo' },
+    { src: 'assets/img/hotel.png', alt: 'Image 4', title: 'Empresa de hotéis de Luxo é destaque 2024', site: 'Globo' },
+    { src: 'assets/img/hotel.png', alt: 'Image 4', title: 'Empresa de hotéis de Luxo é destaque 2024', site: 'Globo' },
+    { src: 'assets/img/hotel.png', alt: 'Image 4', title: 'Empresa de hotéis de Luxo é destaque 2024', site: 'Globo' }
+  ];
 }
